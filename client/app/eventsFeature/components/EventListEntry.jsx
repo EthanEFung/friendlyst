cec24dee3304a8e71b0753beddfb60fb6f541164
Ethan Fung <EthanEFung@gmail.com>
2017-08-16 12:25:34 -0700

    Add buttons and jumbtron to EventsPage

---
+++ client/app/eventsFeature/components/EventListEntry.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EventListEntry extends Component {
+  render() {
+    return (
+      <div className=>
+        Check Out this Event
+      </div>
+    );
+  }
+}
+
+export default EventListEntry;
--- client/app/eventsFeature/components/EventsPage.jsx
+++ client/app/eventsFeature/components/EventsPage.jsx
@@ -6,7 +6,7 @@ import EventList from './EventList.jsx';
 class eventsPage extends Component {
 
   componentDidMount() {
-    
+
   }
 
   render() {
@@ -15,12 +15,14 @@ class eventsPage extends Component {
         <div className="navcopy">
           <Nav />
         </div>
-        <div className="events-page-info">
-          <h1>Events</h1>
-          <br/>
-          <Link to="/create-event-page"><button className="create-event-button">Create Event</button></Link>
+        <div className="events-page-info jumbotron">
+          <h1 className="display-3">Events</h1>
+          <p className="lead">These are the events in your area</p>
+          <button className="event-list-button btn btn-default">See events</button>
+          <Link to="/create-event-page"><button className="btn btn-default">Create Event</button></Link>
+          <hr/>
+          <EventList />
         </div>
-        <EventList />
 
       </div>
     )
