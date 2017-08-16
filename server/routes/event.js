3bd719a24116cfe45bf28492aaf4c06bba1838aa
Ethan Fung <EthanEFung@gmail.com>
2017-08-16 15:30:23 -0700

    Create server route for events

--- server/routes/event.js
+++ server/routes/event.js
@@ -1,2 +1,6 @@
 const router = require('express').Router();
 const controller = require('../controllers/eventCtrl');
+
+router.get('/getAllFriend', controller.getEvents);
+
+module.exports = router;
