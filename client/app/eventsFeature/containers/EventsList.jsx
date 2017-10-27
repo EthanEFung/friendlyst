bea47f32f1c26f006d921b892e590d15c8bc7a8c
Ethan Fung <EthanEFung@gmail.com>
2017-10-27 12:11:48 -0700

    Pretty EventsList.jsx
    Clean up code for better readability.

--- client/app/eventsFeature/containers/EventsList.jsx
+++ client/app/eventsFeature/containers/EventsList.jsx
@@ -1,53 +1,45 @@
-import React, { Component } from 'react';
-import EventListEntry from '../containers/EventListEntry.jsx';
-import { 
-  Col,
-  ListGroup,
-} from 'react-bootstrap';
-import { connect } from 'react-redux';
-import getStoredEvents from '../actions/getStoredEvents';
-import axios from 'axios';
+import React, { Component } from "react";
+import EventListEntry from "../containers/EventListEntry.jsx";
+import { Col, ListGroup } from "react-bootstrap";
+import { connect } from "react-redux";
+import getStoredEvents from "../actions/getStoredEvents";
+import axios from "axios";
 
-const mapStateToProps = (state) => {
-	return {
-		events: state.eventsReducer.events
-	}
-}
+const mapStateToProps = state => {
+  return {
+    events: state.eventsReducer.events
+  };
+};
 
 class EventsList extends Component {
-
   componentDidMount() {
-    axios.get('/api/event/getEvents')
+    axios
+      .get("/api/event/getEvents")
       .then(events => {
-        this.props.getStoredEvents(events.data)
+        this.props.getStoredEvents(events.data);
       })
       .catch(err => {
-        console.log('events page could not receive events from the database', err)
-      })
+        console.log(
+          "events page could not receive events from the database",
+          err
+        );
+      });
   }
-
-  // componentWillReceiveProps() {
-  //   axios.get('/api/event/getEvents')
-  //   .then(events => {
-  //     this.props.getStoredEvents(events.data)
-  //   })
-  //   .catch(err => {
-  //     console.log('events page could not receive events from the database', err)
-  //   })
-  // }
 
   render() {
     return (
       <div>
-        <Col xs={1}/>
+        <Col xs={1} />
 
         <Col xs={10}>
           <ListGroup className="event-list">
-            {this.props.events.map((event) => <EventListEntry key={event.id} {...event}/>)}
+            {this.props.events.map(event => (
+              <EventListEntry key={event.id} {...event} />
+            ))}
           </ListGroup>
         </Col>
-        
-        <Col xs={1}/>
+
+        <Col xs={1} />
       </div>
     );
   }
